a691571b6acfcf7e4adf8e301582554cbf1c3581
Sergey <sdgaykov@gmail.com>
2015-12-17 23:05:24 +0300

    update to es6

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+import React from 'react-native';
+
+const {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity
 } = React;
 
-var pomodoroz = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+class Pomodoroz extends React.Component {
+    constructor(props) {
+        super(props);
 
-var styles = StyleSheet.create({
+        this.state = {
+            timeRemaining: 0,
+            currentProfile: 0,
+            timer: null
+        }
+    }
+
+    changeProfile(min) {
+        this.setState({timeRemaining: min * 60});
+
+        const timer = setInterval(() => {
+            this.setState({timeRemaining: this.state.timeRemaining - 1});
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{this.state.timeRemaining}</Text>
+
+                <TouchableOpacity
+                    onPress={this.changeProfile.bind(this, 25)}
+                >
+                    -----25-----
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
-AppRegistry.registerComponent('pomodoroz', () => pomodoroz);
+AppRegistry.registerComponent('pomodoroz', () => Pomodoroz);
